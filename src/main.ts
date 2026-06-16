598e6c0af9b1ca107986fbc028207892b43ee7e2
Mathew-Pearce <matpearce1984@live.co.uk>
2026-06-16 23:19:13 +0100

    Add player movement and input

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,39 @@
-import Phaser from 'phaser';
+import Phaser from "phaser";
 
 class GameScene extends Phaser.Scene {
+  private player!: Phaser.GameObjects.Rectangle;
+  private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
+
   constructor() {
-    super('GameScene');
+    super("GameScene");
   }
 
   create() {
-    this.add.rectangle(400, 300, 100, 100, 0x7bcff);
-    this.add.text(20, 20, 'spellstorm', {
-      fontSize: '24px',
-      color: '##ffffff',
+    this.player = this.add.rectangle(400, 300, 40, 40, 0x7b5cff);
+
+    this.cursors = this.input.keyboard!.createCursorKeys();
+
+    this.add.text(20, 20, "A Minor Magical Disagreement", {
+      fontSize: "24px",
+      color: "#ffffff",
     });
   }
+
+  update() {
+    const speed = 4;
+
+    if (this.cursors.left.isDown) this.player.x -= speed;
+    if (this.cursors.right.isDown) this.player.x += speed;
+    if (this.cursors.up.isDown) this.player.y -= speed;
+    if (this.cursors.down.isDown) this.player.y += speed;
+  }
 }
+
 new Phaser.Game({
   type: Phaser.AUTO,
   width: 800,
   height: 600,
-  backgroundColor: '#111111',
-  parent: 'app',
+  backgroundColor: "#111111",
+  parent: "app",
   scene: [GameScene],
 });
